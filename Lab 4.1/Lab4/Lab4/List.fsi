324da2f10de3158fc066475ba65f97ca66456f5c
AmerSurkovic <a.surkovic.ba@ieee.org>
2018-03-28 13:57:59 +0200

    Lab 4

---
+++ Lab 4.1/Lab4/Lab4/List.fsi
@@ -0,0 +1,10 @@
+﻿module LIST
+   type List<'Element when 'Element: equality> = Create | Cons of 'Element List * 'Element 
+   val Length : 'Element List -> int
+   val Head : 'Element List -> 'Element
+   val Tail : 'Element List -> 'Element List
+   val GetByIndex : 'Element List * int -> 'Element
+ 
+// TO DO: Add signatures of Count and CircShift here
+   val CircShift : 'Element List * int -> 'Element
+   val Count : 'Element List * int -> int
---
+++ Lab 4.1/Lab4/Lab4/Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.net
+
+module TESTLIST
+   open LIST
+
+   let test = Cons(Cons(Cons(Cons(Create, 3), 2), 3),3)
+   printfn "test = %O" test
+   printfn "test has %i elements" (Length test)
+   printfn "Head(test) = %O" (Head test)
+   printfn "Tail(test) = %O" (Tail test)
+   printfn "Cons(test, GetByIndex(test, 2)) = %O" (Cons(test, GetByIndex(test, 2)))
+   printfn "GetByIndex(test, 3) = %O" (GetByIndex(test, 3))
+   
+   
+   // TO DO: Add some code to test Count and CircShift here
+   printfn "Shifted (test, 1)  = %O" (CircShift(test,1))
+   printfn "Shifted (test, 2)  = %O" (CircShift(test,2))
+   printfn "Shifted (test, 3)  = %O" (CircShift(test,3))
+   printfn "Shifted (test, 4)  = %O" (CircShift(test,4))
+   
+   printfn "(Count (test, 1)) = %O" (Count( test, 1))
+   printfn "(Count (test, 2)) = %O" (Count( test, 2))
+   printfn "(Count (test, 3)) = %O" (Count( test, 3))
+   printfn "(Count (test, 5)) = %O" (Count( test, 5))   
+   
+   printf "Press any key to exit..."
+   ignore (System.Console.ReadKey(true))
